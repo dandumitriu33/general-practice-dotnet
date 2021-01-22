be6e9aec609eeec79df261d260a5225bb4c6129b
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-22 18:14:57 +0200

    Add first test line

---
+++ GeneralPractice/Initial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Initial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
