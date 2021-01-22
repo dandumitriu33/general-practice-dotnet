82da38ee84a57094e8719fdbfae3fac3368ea54c
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-22 19:05:13 +0200

    Add dynamic vs var

---
+++ GeneralPractice/Initial/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Initial
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello {FirstName} {LastName}");
+        }
+    }
+}
--- GeneralPractice/Initial/Program.cs
+++ GeneralPractice/Initial/Program.cs
@@ -6,8 +6,45 @@ namespace Initial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First");
-            Console.WriteLine("Hello World!");
+            // type resolved at runtime
+            // used when in connection with other components from other languages for example
+            dynamic testDynamic = "";
+            var testVar = 2.1;
+
+            testVar = 1.1;
+            //testDynamic.SayHi(); // error at runtime
+
+            testDynamic = 1;
+            testDynamic = testDynamic + 2.1;
+
+            Console.WriteLine(testDynamic);
+
+            dynamic newTestDynamic = new Person();
+            var newTestVar = new Person();
+
+            newTestDynamic.FirstName = "John";
+            newTestDynamic.LastName = "Smith";
+            //newTestDynamic.Email = "jsmith@email.com"; // error at runtime - binder exception
+
+            newTestVar.FirstName = "Mike";
+            newTestVar.LastName = "Doe";
+
+            newTestDynamic.SayHello();
+            newTestVar.SayHello();
+
+            newTestDynamic = "Hi there Jim Jones";
+            Console.WriteLine(newTestDynamic);
+
+            Console.WriteLine(GetMessage());
+
+            // must use var
+            var myCustomer = new { FirstName = "John", Email = "john@email.com" };
+            Console.WriteLine($"Hello {myCustomer.FirstName}, your email is: {myCustomer.Email}");
+        }
+
+        static dynamic GetMessage()
+        {
+            return "This is a test returning a dynamic result.";
         }
     }
 }
