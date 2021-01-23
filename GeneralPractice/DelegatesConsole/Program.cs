77773f692dd323e285f0e1769e799881673391a4
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-23 16:00:19 +0200

    Add Delegates example base code

---
+++ GeneralPractice/DelegatesConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using DelegatesLibrary;
+using System;
+
+namespace DelegatesConsole
+{
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal():C2}");
+
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+    }
+}
