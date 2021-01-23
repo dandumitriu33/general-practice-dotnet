c9fcffe261419fb1f98473dd5ccf54a1d6685b2e
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-23 16:42:44 +0200

    Add Func delegate example

--- GeneralPractice/DelegatesConsole/Program.cs
+++ GeneralPractice/DelegatesConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DelegatesLibrary;
 using System;
+using System.Collections.Generic;
 
 namespace DelegatesConsole
 {
@@ -9,15 +10,34 @@ namespace DelegatesConsole
         static void Main(string[] args)
         {
             PopulateCartWithDemoData();
-
-            decimal total = cart.GenerateTotal(SubTotalAlert);
+            decimal total = cart.GenerateTotal(SubTotalAlert,
+                                               CalculateLeveledDiscount);
             Console.WriteLine($"The total for the cart is {total:C2}");
-
         }
 
         private static void SubTotalAlert(decimal subTotal)
         {
             Console.WriteLine($"The subtotal is {subTotal:C2}");
+        }
+
+        private static decimal CalculateLeveledDiscount(List<ProductModel> items, decimal subTotal)
+        {
+            if (subTotal > 100)
+            {
+                return subTotal * 0.80M;
+            }
+            else if (subTotal > 50)
+            {
+                return subTotal * 0.85M;
+            }
+            else if (subTotal > 10)
+            {
+                return subTotal * 0.95M;
+            }
+            else
+            {
+                return subTotal;
+            }
         }
 
         private static void PopulateCartWithDemoData()
--- GeneralPractice/DelegatesLibrary/ShoppingCartModel.cs
+++ GeneralPractice/DelegatesLibrary/ShoppingCartModel.cs
@@ -10,28 +10,15 @@ namespace DelegatesLibrary
         public delegate void MentionDiscount(decimal subTotal);
         public List<ProductModel> Items { get; set; } = new List<ProductModel>();
 
-        public decimal GenerateTotal(MentionDiscount mentionDiscount)
+        public decimal GenerateTotal(MentionDiscount mentionDiscount,
+                                Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal)
         {
             decimal subTotal = Items.Sum(x => x.Price);
             // the method we pass in will be used here - like a variable
             mentionDiscount(subTotal);
 
-            if (subTotal > 100)
-            {
-                return subTotal * 0.80M;
-            }
-            else if (subTotal > 50)
-            {
-                return subTotal * 0.85M;
-            }
-            else if (subTotal > 10)
-            {
-                return subTotal * 0.90M;
-            }
-            else
-            {
-                return subTotal;
-            }
+            decimal total = calculateDiscountedTotal(Items, subTotal);
+            return total;
         }
     }
 }
