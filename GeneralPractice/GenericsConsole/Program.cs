12d91abb910d5706a4156321e0e483b391c726d4
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-24 18:20:36 +0200

    Add Generics console app

---
+++ GeneralPractice/GenericsConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
