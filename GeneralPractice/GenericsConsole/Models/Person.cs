19507f54504ef595607b3c3f7299bf9e4e84b263
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-24 19:05:29 +0200

    Add Generics exer non-generic example

---
+++ GeneralPractice/GenericsConsole/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsConsole.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAlive { get; set; } = true;
+    }
+}
--- GeneralPractice/GenericsConsole/Program.cs
+++ GeneralPractice/GenericsConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GenericsConsole.Models;
+using GenericsConsole.WithoutGenerics;
+using System;
+using System.Collections.Generic;
 
 namespace GenericsConsole
 {
@@ -6,7 +9,45 @@ namespace GenericsConsole
     {
         static void Main(string[] args)
         {
+            DemonstrateTextFileStorage();
+
             Console.WriteLine("Hello World!");
+        }
+
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new List<Person>();
+            List<LogEntry> logs = new List<LogEntry>();
+
+            string peopleFile = @"C:\Users\Dan\Projects\general-practice-dotnet\GeneralPractice\GenericsFiles\people.csv";
+            string logFile = @"C:\Users\Dan\Projects\general-practice-dotnet\GeneralPractice\GenericsFiles\logs.csv";
+
+            PopulateLists(people, logs);
+
+            OriginalTextFileProcessor.SaveLogs(logs, logFile);
+            var newLogs = OriginalTextFileProcessor.LoadLogs(logFile);
+            foreach (var log in newLogs)
+            {
+                Console.WriteLine($"{log.ErrorCode}: {log.Message} at {log.TimeOfEvent.ToShortTimeString()}");
+            }
+
+            //OriginalTextFileProcessor.SavePeople(people, peopleFile);
+            //var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+            //foreach (var p in newPeople)
+            //{
+            //    Console.WriteLine($"{p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+            //}
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", IsAlive = false });
+            people.Add(new Person { FirstName = "John", LastName = "Smith" });
+
+            logs.Add(new LogEntry { Message = "Something happened", ErrorCode = 9999 });
+            logs.Add(new LogEntry { Message = "Awesome", ErrorCode = 1337 });
+            logs.Add(new LogEntry { Message = "Something else", ErrorCode = 2222 });
         }
     }
 }
